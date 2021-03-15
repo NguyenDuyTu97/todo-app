0dc61343be7a2b42d8c336b7eca61c8523e8907c
NguyenDuyTu97 <45293691+NguyenDuyTu97@users.noreply.github.com>
2021-03-15 23:16:38 +0700

    Create index.js

---
+++ src/modules/todo/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoApp.propTypes = {
+
+};
+
+function TodoApp(props) {
+    return (
+        <div>
+            This is todo app
+        </div>
+    );
+}
+
+export default TodoApp;
